94fe0ec41d2f20c14590e986149dd5771bd174c4
Rik Smit <rik.smit2912@gmail.com>
2021-03-09 15:36:22 +0100

    Adds jumping

--- ApplePie/Assets/Scripts/Manager.cs
+++ ApplePie/Assets/Scripts/Manager.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.Users;
 
 public class Manager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject archer;
+    [SerializeField] private GameObject bokser;
+    //[SerializeField] private GameObject tank;
+
+    private bool archerIsSpawned;
+    private bool bokserIsSpawned;
+    private bool tankIsSpawned;
+    bool isPressed;
+
+    public const string PlayerJoinedMessage = "OnPlayerJoined";
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
-
-    // Update is called once per frame
-    void Update()
+    void Archer()
+    {
+      
+    }
+    void Bokser()
     {
         
+       
+    }
+    public void EnableJoining()
+    {
+     
+   }
+    
+   // Update is called once per frame
+   void Update()
+   {
+
+
+
     }
 }
+
--- ApplePie/Assets/Scripts/PlayerMovement.cs
+++ ApplePie/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,25 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     //public float speed = 5;
+    public int forceConst = 20;
+
+    private bool canJump;
+    private Rigidbody selfRigidbody;
+
     private Vector2 movementInput;
-    [SerializeField] private float speed = 5f;
+    private Vector2 jumpInput;
+
+[SerializeField] private float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        selfRigidbody = GetComponent<Rigidbody>();
     }
-
+    public void OnPlayerJoined(InputAction.CallbackContext context)
+    {
+        Debug.Log("works");
+    }
     // Update is called once per frame
     void CheckClass()
     {
@@ -21,12 +32,30 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
+
         transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+
+        if (canJump)
+        {
+            canJump = false;
+            selfRigidbody.AddForce(0, forceConst, 0,ForceMode.Impulse);
+        }
+
     }
-    public void OnMove(InputAction.CallbackContext ctx)
+    public void OnMoving(InputAction.CallbackContext ctx)
     {
         movementInput = ctx.ReadValue<Vector2>();
     }
-    
-    
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (Gamepad.current.buttonSouth.isPressed)
+        {
+            Debug.Log("Pressedsouth");
+            canJump = true;
+        }
+    }
+
+   
+
+
 }
