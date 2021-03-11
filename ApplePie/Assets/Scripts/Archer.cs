3cd3bdfa575060ba86fe9c882dd290cb9cbe9617
Rik Smit <rik.smit2912@gmail.com>
2021-03-11 15:33:01 +0100

    Adds smooth shooting with addforce

--- ApplePie/Assets/Scripts/Archer.cs
+++ ApplePie/Assets/Scripts/Archer.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Archer : MonoBehaviour
 {
+    
     public GameObject arrow;
     public bool isShooting;
+    public Transform barrel;
+    [SerializeField] private int arrowSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,26 +19,32 @@ public class Archer : MonoBehaviour
     {
         arrow.SetActive(true);
         isShooting = true;
-        yield return new WaitForSeconds(2);
+        GameObject newArrow = Instantiate(arrow,barrel.position,barrel.rotation);
+        newArrow.GetComponent<Rigidbody>().AddForce(newArrow.transform.forward * arrowSpeed);
+        Destroy(newArrow, 1.5f);
+        yield return new WaitForSeconds(1.5f);
         isShooting = false;
         arrow.SetActive(false);
-        arrow.transform.position = transform.localPosition;
+        arrow.transform.position = transform.localPosition + new Vector3(-0.3f,0,0);
     }
     // Update is called once per frame
     void Update()
     {
-        if (isShooting)
+       /* if (isShooting)
         {
-            arrow.transform.Translate(Vector3.forward * 3 * Time.deltaTime);
+            arrow.transform.Translate(new Vector3(3,transform.rotation.y,0) * Time.deltaTime);
             arrow.transform.parent = null;
         }
         else if(!isShooting)
         {
             arrow.transform.parent = transform;
-        }
+        }*/
     }
     public void OnArrowShoot()
     {
-        StartCoroutine(ArrowShooting());
+        if (!isShooting)
+        {
+            StartCoroutine(ArrowShooting());
+        }
     }
 }
