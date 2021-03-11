6675b1c7f1e77f09cb0e6b943ad93cd70bcc0bbb
Rik Smit <rik.smit2912@gmail.com>
2021-03-11 15:00:07 +0100

    Adds basic shooting

---
+++ ApplePie/Assets/Scripts/Archer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+    public GameObject arrow;
+    public bool isShooting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isShooting = false;
+        arrow.SetActive(false);
+    }
+    IEnumerator ArrowShooting()
+    {
+        arrow.SetActive(true);
+        isShooting = true;
+        yield return new WaitForSeconds(2);
+        isShooting = false;
+        arrow.SetActive(false);
+        arrow.transform.position = transform.localPosition;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isShooting)
+        {
+            arrow.transform.Translate(Vector3.forward * 3 * Time.deltaTime);
+            arrow.transform.parent = null;
+        }
+        else if(!isShooting)
+        {
+            arrow.transform.parent = transform;
+        }
+    }
+    public void OnArrowShoot()
+    {
+        StartCoroutine(ArrowShooting());
+    }
+}
