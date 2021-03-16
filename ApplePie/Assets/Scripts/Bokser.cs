a0cfc5e17316edc5d259fe3291e6edba9f44010f
Rik Smit <rik.smit2912@gmail.com>
2021-03-16 15:30:58 +0100

    adds a version of the first abbility + 3d art implemented

---
+++ ApplePie/Assets/Scripts/Bokser.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bokser : MonoBehaviour
+{
+    public GameObject ab1;
+
+    private bool isDoingAb1 = false;
+    private Rigidbody r;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();
+        ab1.SetActive(false);
+    }
+    IEnumerator StartAb1()
+    {
+        isDoingAb1 = true;
+        ab1.SetActive(true);
+        yield return new WaitForSeconds(3);
+        ab1.SetActive(false);
+        isDoingAb1 = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDoingAb1)
+        {
+            
+        }
+    }
+    public void AbbilitieOne()
+    {
+        if (!isDoingAb1)
+        {
+            StartCoroutine(StartAb1());
+        }
+    }
+    public void AbbilitieTwo()
+    {
+
+    }
+}
