950c5fc90e77fb137f94d8c8a2d1d10eb62a70b8
Rik Smit <rik.smit2912@gmail.com>
2021-03-22 10:45:53 +0100

    Adds an audio player

---
+++ ApplePie/Assets/Scripts/Music.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource mainTheme;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainTheme = GetComponent<AudioSource>();
+        mainTheme.Play(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
