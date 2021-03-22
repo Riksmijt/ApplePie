c6ff3e29e29e1159ecdbf741e01757cda1db1ddb
Rik Smit <rik.smit2912@gmail.com>
2021-03-22 11:00:12 +0100

    adds the ability to play clips when the music is still playing

--- ApplePie/Assets/Scripts/Music.cs
+++ ApplePie/Assets/Scripts/Music.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Music : MonoBehaviour
 {
     public AudioSource mainTheme;
-    
+    public AudioClip aButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,9 @@ public class Music : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Gamepad.current.aButton.wasPressedThisFrame)
+        {
+            mainTheme.PlayOneShot(aButton);
+        }
     }
 }
