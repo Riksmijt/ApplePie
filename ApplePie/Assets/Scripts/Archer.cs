92f925673c78008f0b5cabab9e4ac9dd10198011
Rik Smit <rik.smit2912@gmail.com>
2021-03-24 15:29:14 +0100

    adds shooting to the game

--- ApplePie/Assets/Scripts/Archer.cs
+++ ApplePie/Assets/Scripts/Archer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Archer : MonoBehaviour
 {
@@ -8,57 +9,39 @@ public class Archer : MonoBehaviour
     private bool isShooting;
     private float timer;
     private int amountArrows;
+    private float shotTimer;
     // Start is called before the first frame update
     void Start()
     {
         isShooting = false;
-        arrow.SetActive(false);
         timer = 0;
         amountArrows = 2;
+        shotTimer = 3;
     }
-    IEnumerator ArrowShooting()
-    {
-        isShooting = true;
-        arrow.SetActive(true);
-        yield return new WaitForSeconds(2);
-        isShooting = false;
-
-        //arrow.transform.position = transform.localPosition + new Vector3(-0.3f,0,0);
-
-        arrow.SetActive(false);
-        arrow.transform.position = transform.localPosition;
-
-    }
+    
     // Update is called once per frame
     void Update()
     {
-        if (isShooting)
-        {
-            arrow.transform.Translate(Vector3.back * 30 * Time.deltaTime);
-            arrow.transform.parent = null;
-        }
-        if(amountArrows <= 0)
-        {
-            timer += 1 * Time.deltaTime;
-            if(timer >= 3)
-            {
-                timer = 0;
-                amountArrows = 2;
-            }
-        }
-        
-        else if(!isShooting)
-        {
-            arrow.transform.parent = transform;
-        }
-        Debug.Log(timer);
+
+            shotTimer -= 1 * Time.deltaTime;
+
     }
-    public void OnArrowShoot()
+    public void OnArrowShoot(InputAction.CallbackContext ctx)
     {
-        if (amountArrows >= 2)
+        if (amountArrows >= 1 && shotTimer <= 0)
         {
+            GameObject newArrow = Instantiate(arrow, transform.position + transform.forward * 2,transform.rotation);
+            newArrow.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+            Destroy(newArrow, 2);
             isShooting = true;
             amountArrows -= 1;
+            shotTimer = 3;
+            if (amountArrows <= 0)
+            {
+                amountArrows = 2;
+
+                
+            }
         }
     }
 }
