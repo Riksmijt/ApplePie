a50ead884e9c1566dec22f1e7985ebecf80c8b20
Rik Smit <rik.smit2912@gmail.com>
2021-03-25 10:29:32 +0100

    changed the 2 bokserAB scripts to 1 BokserAbility

--- ApplePie/Assets/Scripts/BokserAbility.cs
+++ ApplePie/Assets/Scripts/BokserAbility.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class BokserAb1 : MonoBehaviour
 {
+    [SerializeField] private float force;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.tag == "Bokser")
         {
-            collision.transform.GetComponent<Rigidbody>().AddForce(Vector3.back * 100000);
+            collision.transform.GetComponent<Rigidbody>().AddForce(Vector3.back * force);
             PlayerMovement.playerHealth += 1;
         }
     }
