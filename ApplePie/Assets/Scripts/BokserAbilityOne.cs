c63d8a1bc3786b8f0be7561d9d6bde9e4e45e125
rikdmit <rik.smit2912@gmail.com>
2021-04-23 11:54:04 +0200

    Updates the first ability of the bokser

--- ApplePie/Assets/Scripts/BokserAbilityOne.cs
+++ ApplePie/Assets/Scripts/BokserAbilityOne.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class BokserAbilityOne : MonoBehaviour
 {
     [SerializeField] private float cooldownTimer;
+    [SerializeField] private GameObject forceField;
     private float timer;
+    private float abilityTimer = 3;
+    private bool isDoingAbility;
     public void ActivateAbility()
     {
         if (timer <= 0)
         {
+            isDoingAbility = true;
             timer = cooldownTimer;
             Debug.Log("Activated ability one");
         }
@@ -18,5 +22,16 @@ public class BokserAbilityOne : MonoBehaviour
     private void Update()
     {
         timer -= 1 * Time.deltaTime;
+        if (isDoingAbility)
+        {
+            forceField.SetActive(true);
+            abilityTimer -= 1 * Time.deltaTime;
+        }
+        if(abilityTimer <= 0)
+        {
+            forceField.SetActive(false);
+            isDoingAbility = false;
+            abilityTimer = 3;
+        }
     }
 }
